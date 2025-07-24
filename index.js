d65c3ae51e39855aaf938a55e3c7bac4b95e0bd9
Hamxa Ameer <hamxaameer42@gmail.com>
2025-07-24 15:18:49 +0500

    using dummy data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import './polyfills';
+import './src/polyfills';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
---
+++ src/polyfills.js
@@ -0,0 +1,38 @@
+import 'react-native-get-random-values';
+import { decode, encode } from 'base-64';
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
+
+// Polyfill for crypto.getRandomValues
+if (typeof __dirname === 'undefined') global.__dirname = '/';
+if (typeof __filename === 'undefined') global.__filename = '';
+if (typeof process === 'undefined') {
+  global.process = require('process');
+} else {
+  const bProcess = require('process');
+  for (var p in bProcess) {
+    if (!(p in process)) {
+      process[p] = bProcess[p];
+    }
+  }
+}
+
+global.Buffer = require('@craftzdog/react-native-buffer').Buffer;
+global.process.browser = false;
+if (typeof btoa === 'undefined') {
+  global.btoa = function (str) {
+    return new Buffer(str, 'binary').toString('base64');
+  };
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = function (b64Encoded) {
+    return new Buffer(b64Encoded, 'base64').toString('binary');
+  };
+}
